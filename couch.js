fcbaaa27fde1a7a62352ef15aa513277321db5df
Brandon <brandon.leonardo@gmail.com>
2010-08-28 10:28:09 -0700

    test using couch.io

--- couch.js
+++ couch.js
@@ -1,7 +1,7 @@
 var sys = require('sys'),
     fs = require('fs'),
     cradle = require('cradle');
-var c = new(cradle.Connection);
+var c = new(cradle.Connection)('maprejuice.couchone.com');
 sys.puts('Got c => ' + sys.inspect(c));
 sys.puts('Databases => '+c.databases());
 var db = c.database('maprejuice');
@@ -26,6 +26,7 @@ db.get('b30fba2e6b5beb795f1fed91f6000c12', function (err, doc) {
 });
 sys.puts('Got p');
 
+/*
 db.insert({
     name: 'Problem 2',
     map_function: "function () { ; }",
@@ -36,6 +37,7 @@ db.insert({
     sys.puts('db.insert => res = '+sys.inspect(res));
 });
 sys.puts('Set p');
+*/
 
 db.get('problem1', function (err, doc) {
     sys.puts('db.get(problem1) => doc = '+doc);
